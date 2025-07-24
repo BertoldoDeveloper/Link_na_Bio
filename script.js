ca99f29894243a2078a2e5ac91ed3425a74f7b5a
Ladislau Bertoldo <bertoldo.developer@gmail.com>
2025-07-24 10:14:49 -0300

    Feat - Adicionando a pagina do JavaScript, com as funcionalidades de encaminhamento de pagina

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function linkInstagram() {
+    window.location.href = 'https://www.instagram.com/_bertoldo_/#'
+}
+
+function linkGitHub() {
+    window.location.href = 'https://github.com/BertoldoDeveloper'
+}
+
+function linkLinkedin() {
+    window.location.href = 'https://www.linkedin.com/in/ladislau-bertoldo/'
+}
+
+function linkContato() {
+    window.location.href = 'mailto:bertoldo.developer@gmail.com'
+}
